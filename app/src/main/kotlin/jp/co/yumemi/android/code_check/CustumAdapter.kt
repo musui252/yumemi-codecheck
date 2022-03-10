21b450483480e7f202efc97bd2cd332a5a49ef01
musui252 <symphogear252@gmail.com>
2022-03-10 18:30:59 +0900

    custumAdapterの切り出し
    クラスファイル追加

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/CustumAdapter.kt
@@ -0,0 +1,51 @@
+package jp.co.yumemi.android.code_check
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+// リストの差分を検知する比較関数のオブジェクト
+val diff_util = object : DiffUtil.ItemCallback<Item>() {
+    override fun areItemsTheSame(oldItem: Item, newItem: Item): Boolean {
+        return oldItem.name == newItem.name
+    }
+
+    override fun areContentsTheSame(oldItem: Item, newItem: Item): Boolean {
+        return oldItem == newItem
+    }
+
+}
+
+// リポジトリ検索のサジェストの生成と更新
+class CustomAdapter(
+    private val itemClickListener: OnItemClickListener
+) : ListAdapter<Item, CustomAdapter.ViewHolder>(diff_util) {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
+    interface OnItemClickListener {
+        fun itemClick(item: Item)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.layout_item, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = getItem(position)
+        val repositoryNameView =
+            holder.itemView.findViewById<View>(R.id.repositoryNameView) as TextView
+        repositoryNameView.text = item.name
+
+        holder.itemView.setOnClickListener {
+            itemClickListener.itemClick(item)
+        }
+    }
+}
